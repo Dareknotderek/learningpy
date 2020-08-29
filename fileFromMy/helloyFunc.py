37aeb65971b06e041cab4d3c37ccbae9620ebc58
Dareknotderek <32199587+Dareknotderek@users.noreply.github.com>
2020-08-29 10:55:48 -0500

    Add files via upload

---
+++ fileFromMy/helloyFunc.py
@@ -0,0 +1,8 @@
+#first lesson on how to define functions
+def hello():
+    print('Howdy!')
+    print('Howdy!!')
+    print('Hello there.')
+hello()
+hello()
+hello()
---
+++ fileFromMy/helloyFunc2.py
@@ -0,0 +1,5 @@
+#Second defining function exercise, allows for arguments and parameters
+def hello(name):
+    print('Hello, ' + name)
+hello('Alice')
+hello('Bob')
