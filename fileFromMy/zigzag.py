fe0ccdfa88c3d658e29563e1c1690eeb1b70f0fc
Dareknotderek <32199587+Dareknotderek@users.noreply.github.com>
2020-09-02 14:15:31 -0500

    Update zigzag.py

--- fileFromMy/zigzag.py
+++ fileFromMy/zigzag.py
@@ -1,25 +1,26 @@
 #makes a program that moves a few astericks back and forth until `cntrl-c` is keyed
 import time, sys
-indent=0 #how many spaces to indent
-indentIncreasing=True #whether the indentation or increasing or not
+indent = 0 # How many spaces to indent.
+indentIncreasing = True # Whether the indentation is increasing or not.
 
 try:
-    while True: #the mail program loop.
+    while True: # The main program loop.
         print(' ' * indent, end='')
         print('********')
-        time.sleep(0.1) #pause for 1/10 of a second.
+        time.sleep(0.1) # Pause for 1/10 of a second.
 
-    if indentIncreasing:
-        #increase the number of space:
-        indent = indent+1
-        if indent == 20:
-            #change direction
-            indentIncreasing=False
-    else:
-        #decrease the number of spaces:
-        indent = indent - 1
-        if indent==0:
-            #change direction
-            indentIncreasing=True
-except KeyboardInterupt:
+        if indentIncreasing:
+            # Increase the number of spaces:
+            indent = indent + 1
+            if indent == 20:
+                # Change direction:
+                indentIncreasing = False
+
+        else:
+            # Decrease the number of spaces:
+            indent = indent - 1
+            if indent == 0:
+                # Change direction:
+                indentIncreasing = True
+except KeyboardInterrupt:
     sys.exit()
