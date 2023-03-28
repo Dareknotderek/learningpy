d1ee326dc60bbd3eb4cd31f6a21c42e94ffb6c0b
Dareknotderek <32199587+Dareknotderek@users.noreply.github.com>
2023-03-27 20:07:06 -0500

    Update magic8Ball2.py
    Changed the variable name messages to responses to better reflect the purpose of the list.
    Fixed the capitalization of the second response in the list for consistency.
    Used random.choice() instead of random.randint() to simplify the random selection of a response from the list.
    Encapsulated the main functionality into a magic_8_ball() function for better code organization and reusability.
    Added a check for __name__ == "__main__" to allow the script to be imported as a module without executing the function immediately.

--- fileFromMy/magic8Ball2.py
+++ fileFromMy/magic8Ball2.py
@@ -1,17 +1,21 @@
-#Makes a more elegant version of `magic8Ball.py` by using lists
-#removes need to use multiple identical `elif` statements
+import random
 
-import random #imports random so we can have "random" outputs from asking the 8 ball
-
-messages = ['It is certain', #list that has the messages from the 8 ball
-    'it is decidely so',
+# A list containing the responses from the magic 8 ball
+responses = [
+    'It is certain',
+    'It is decidedly so',
     'Yes, definitely',
     'Reply hazy, try again',
     'Ask again later',
     'Concentrate and ask again',
     'My reply is no',
     'Outlook not so good',
-    'Very doubtful']
+    'Very doubtful'
+]
 
-print(messages[random.randint(0, len(messages)-1)]) #looks at list of messages, randint picks
-#an index number between 0 and the length of messages minus one (so we don't get an error)
+def magic_8_ball():
+    response = random.choice(responses)  # Selects a random response from the list
+    print(response)
+
+if __name__ == "__main__":
+    magic_8_ball()
